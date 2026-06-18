afb06b504136a66307261ca861fea48ea30645a3
Badass Data Science <badassdatascience@gmail.com>
2026-06-18 07:03:22 -0700

    Added a function for querying to a DataFrame.

--- python_tools_and_shortcuts/databases/neo4j/Neo4jInterface.py
+++ python_tools_and_shortcuts/databases/neo4j/Neo4jInterface.py
@@ -9,19 +9,21 @@ class Neo4jInterface():
         uri = 'neo4j://localhost:7687',         
         username = 'neo4j',
     ):
-        self.auth = (username, password)
+        auth = (username, password)
         self.database = database
-        self.driver = GraphDatabase.driver(uri, auth = self.auth)
+        self.driver = GraphDatabase.driver(uri, auth = auth)
 
-    #
-    # need to confirm this actually works
-    #
+    def query_to_df(self, query):
+        df = self.driver.execute_query(
+            query,
+            database_ = self.database,
+            result_transformer_ = Result.to_df
+        )
+        return df
+    
     def drop_everything(self):
         self.driver.execute_query('MATCH (n) DETACH DELETE n', database = self.database)
 
-    #
-    # need to confirm this actually works
-    #
     def drop_everything_by_node_type(self, the_type):
         self.driver.execute_query('MATCH (n:' + the_type + ') DETACH DELETE n', database = self.database)
     
