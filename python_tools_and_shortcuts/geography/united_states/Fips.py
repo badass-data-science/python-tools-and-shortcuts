1575fc4c7ac0cd3bf5148988c982b212bd0c16f0
Badass Data Science <badassdatascience@gmail.com>
2026-06-22 15:37:21 -0700

    Improved the documentation.

--- python_tools_and_shortcuts/geography/united_states/Fips.py
+++ python_tools_and_shortcuts/geography/united_states/Fips.py
@@ -1,7 +1,19 @@
+# Federal Information Processing Standard (FIPS) uniquely identify
+# U.S. states, territories, and counties. These are standardized
+# across the United States government and managed by NIST.
+
+#
+# Load useful libraries
+#
 import pandas as pd
 from datetime import datetime, timezone
 
+#
+# Define a parent class for containing FIPS data
+#
 class Fips():
+
+    # constructor
     def __init__(
         self,
         url_fips_codes = 'https://www2.census.gov/geo/docs/reference/codes2020/national_county2020.txt',
@@ -9,16 +21,23 @@ class Fips():
         self.url_fips_codes = url_fips_codes
         self.timestamp_download = None
 
+    # save the timestamp when FIPS content download occured
     def set_download_timestamp(self):
         self.timestamp_download = datetime.now(timezone.utc)
-        
+
+#
+# Define a child class for working with county-level FIPS codes
+#
 class FipsCounty(Fips):
+
+    # constructor
     def __init__(
         self,
         url_fips_codes = 'https://www2.census.gov/geo/docs/reference/codes2020/national_county2020.txt',
     ):
         super().__init__(url_fips_codes)
-    
+
+    # download the FIPS codes and store them per county
     def download_FIPS_codes(self):
         self.df = (
             pd.read_csv(
@@ -37,13 +56,19 @@ class FipsCounty(Fips):
         )
         self.set_download_timestamp()
 
+#
+# Define a child class for working with state-level FIPS codes
+#
 class FipsState(Fips):
+
+    # constructor
     def __init__(
         self,
         url_fips_codes = 'https://www2.census.gov/geo/docs/reference/codes2020/national_county2020.txt',
     ):
         super().__init__(url_fips_codes)
-    
+
+    # download the FIPS codes and store them per state
     def download_FIPS_codes(self):
         self.df = (
             pd.read_csv(
